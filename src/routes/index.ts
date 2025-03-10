ad73a3e32d4233b8ce7f68bba99397a416dda3de
warley farias <157419864+warrley@users.noreply.github.com>
2025-03-10 10:41:01 -0300

    add contact

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,30 @@
 import { Router } from 'express'
+import { readFile, writeFile } from 'fs/promises';
+
+const dataSource = "./data/list.txt"
 
 export const router = Router();
 
-router.get("/users", (req, res) => {
-    res.json("server online");
+router.post("/contact", async (req, res) => {
+    const name = req.body.name;
+
+    if (!name || name.length < 2) {
+        res.json({ error: "insert at least 2 characters." });
+        return;
+    }
+
+    let list: string[] = [];
+
+    try {
+        const data = await readFile(dataSource, { encoding: "utf-8" });
+        list = data.split("\n")
+    } catch (err) {
+        
+    }
+
+    list.push(name);
+
+    await writeFile(dataSource, list.join("\n"));
+
+    res.status(201).json({ contact: name });
 });
